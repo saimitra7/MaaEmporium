db5a79bf88850408f9d35719586a0b96b98aed2b
saimitra7 <mitrasai995@gmail.com>
2024-11-16 23:04:42 +0530

    latest commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { addCart } from "../redux/action";
 
@@ -12,7 +12,8 @@ const Products = () => {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
   const [loading, setLoading] = useState(false);
-  let componentMounted = true;
+  
+  const componentMounted = useRef(true); // using useRef to persist value across renders
 
   const dispatch = useDispatch();
 
@@ -24,18 +25,20 @@ const Products = () => {
     const getProducts = async () => {
       setLoading(true);
       const response = await fetch("https://fakestoreapi.com/products/");
-      if (componentMounted) {
-        setData(await response.clone().json());
-        setFilter(await response.json());
+      if (componentMounted.current) {
+        const products = await response.json();
+        setData(products);
+        setFilter(products);
         setLoading(false);
       }
-
-      return () => {
-        componentMounted = false;
-      };
     };
 
     getProducts();
+
+    // Cleanup function to set `componentMounted` to false when the component unmounts
+    return () => {
+      componentMounted.current = false;
+    };
   }, []);
 
   const Loading = () => {
@@ -44,24 +47,11 @@ const Products = () => {
         <div className="col-12 py-5 text-center">
           <Skeleton height={40} width={560} />
         </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
-        <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
-          <Skeleton height={592} />
-        </div>
+        {[...Array(7)].map((_, index) => (
+          <div className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4" key={index}>
+            <Skeleton height={592} />
+          </div>
+        ))}
       </>
     );
   };
@@ -75,89 +65,55 @@ const Products = () => {
     return (
       <>
         <div className="buttons text-center py-5">
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => setFilter(data)}
-          >
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => setFilter(data)}>
             All
           </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("men's clothing")}
-          >
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("men's clothing")}>
             Men's Clothing
           </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("women's clothing")}
-          >
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("women's clothing")}>
             Women's Clothing
           </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("jewelery")}
-          >
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("jewelery")}>
             Jewelery
           </button>
-          <button
-            className="btn btn-outline-dark btn-sm m-2"
-            onClick={() => filterProduct("electronics")}
-          >
+          <button className="btn btn-outline-dark btn-sm m-2" onClick={() => filterProduct("electronics")}>
             Electronics
           </button>
         </div>
 
-        {filter.map((product) => {
-          return (
-            <div
-              id={product.id}
-              key={product.id}
-              className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
-            >
-              <div className="card text-center h-100" key={product.id}>
-                <img
-                  className="card-img-top p-3"
-                  src={product.image}
-                  alt="Card"
-                  height={300}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    {product.title.substring(0, 12)}...
-                  </h5>
-                  <p className="card-text">
-                    {product.description.substring(0, 90)}...
-                  </p>
-                </div>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item lead">₹ {product.price}</li>
-                  {/* <li className="list-group-item">Dapibus ac facilisis in</li>
-                    <li className="list-group-item">Vestibulum at eros</li> */}
-                </ul>
-                <div className="card-body">
-                  <Link
-                    to={"/product/" + product.id}
-                    className="btn btn-dark m-1"
-                  >
-                    Buy Now
-                  </Link>
-                  <button
-                    className="btn btn-dark m-1"
-                    onClick={() => {
-                      toast.success("Added to cart");
-                      addProduct(product);
-                    }}
-                  >
-                    Add to Cart
-                  </button>
-                </div>
+        {filter.map((product) => (
+          <div key={product.id} className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4">
+            <div className="card text-center h-100">
+              <img className="card-img-top p-3" src={product.image} alt="Card" height={300} />
+              <div className="card-body">
+                <h5 className="card-title">{product.title.substring(0, 12)}...</h5>
+                <p className="card-text">{product.description.substring(0, 90)}...</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item lead">₹ {product.price}</li>
+              </ul>
+              <div className="card-body">
+                <Link to={"/product/" + product.id} className="btn btn-dark m-1">
+                  Buy Now
+                </Link>
+                <button
+                  className="btn btn-dark m-1"
+                  onClick={() => {
+                    toast.success("Added to cart");
+                    addProduct(product);
+                  }}
+                >
+                  Add to Cart
+                </button>
               </div>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </>
     );
   };
+
   return (
     <>
       <div className="container my-3 py-3">
