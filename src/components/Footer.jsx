222e0061a309921e033bb617706bcf8b27989fa9
saimitra7 <mitrasai995@gmail.com>
2024-11-20 23:50:46 +0530

    update: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,7 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="www.github.com/saimitra07" className="" target="" rel="">Sai Mitra</a>
+            <p className="mb-3 mb-md-0">Made with ❤️ by SaiMitra
             </p>
             <a className="text-dark fs-4" href="https://github.com/saimitra7" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
